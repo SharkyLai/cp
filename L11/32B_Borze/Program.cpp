248025c37b8845d891f24193a3ce2f3f985aeaca
Sharky <71050482+SharkyLai@users.noreply.github.com>
2021-05-01 11:06:02 +0800

    finish 32b

--- L11/32B_Borze/Program.cpp
+++ L11/32B_Borze/Program.cpp
@@ -1,6 +1,26 @@
+// Digit 0 is transmitted as «.», 1 as «-.» and 2 as «--».
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    string s, s2;
+    cin >> s;
+    int length = s.length();
+    
+    for (int i = 0; i < length; i++) {
+        if (s[i] == '.') {
+            s2 += '0';
+        }
+        if (s[i] == '-' && s[i + 1] == '.') {
+            s2 += '1';
+            ++i;
+        }
+        if (s[i] == '-' && s[i + 1] == '-') {
+            s2 += '2';
+            ++i;
+        }
+    }
+    cout << s2 << endl;
 }
